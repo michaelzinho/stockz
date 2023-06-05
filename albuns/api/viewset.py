f289eaf526cc09ba7874d7e69f49e9780e75d984
Jonathan Nicolas <weptpear394@gmail.com>
2023-06-05 04:51:27 -0300

    implementation of photos, albums and static image support

---
+++ albuns/api/viewset.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers, viewsets
+from ..models import Album
+from .serializers import AlbumSerializers
+
+class AlbumViewSet (viewsets.ModelViewSet):
+
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializers
+    
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from users.API.views.GoogleLoginView import GoogleLoginView, UserRedirectView
+from rest_framework.schemas  import get_schema_view
+from rest_framework.routers import DefaultRouter
+from django.conf.urls.static import static
+from django.conf import settings
 
 from drf_spectacular.views import (
     SpectacularAPIView, 
@@ -8,18 +12,33 @@ from drf_spectacular.views import (
     SpectacularSwaggerView
 )
 
+from albuns.api.viewset import AlbumViewSet
+from photos.api.viewset import PhotoViewSet, MultiplePhotosViewSet
+
+
+router = DefaultRouter()
+router.register(r'Album', AlbumViewSet, basename='Album')
+router.register(r'SinglePhoto', PhotoViewSet, basename='Photo')
+router.register(r'MultiplePhotos', MultiplePhotosViewSet, basename='Photos')
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    
     #dj rest auth | authentication
     path('user/', include('dj_rest_auth.urls')),
     path('user/registration/', include('dj_rest_auth.registration.urls')),
 
     #dj rest auth | social authentication
     path('', include('users.urls')),
+    
+    #api
+    path('api/', include(router.urls)),
+    
     #spectacular | docs
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/doc/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
